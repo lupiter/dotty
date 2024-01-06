8a041aa46eb69dc44d31d9c23960d40397567cf1
Sir Cathy W <everbloom@gmail.com>
2024-01-06 14:08:08 +1100

    tests be good

--- src/document/canvas-controler.test.ts
+++ src/document/canvas-controler.test.ts
@@ -1,30 +1,83 @@
-import { expect, test, vi } from "vitest";
-import { CanvasController, CanvasProps, CanvasState } from "./canvas-controler";
+import { describe, expect, test, vi } from "vitest";
+import { CanvasController, CanvasState } from "./canvas-controler";
+import { Point } from "../color/geometry";
 
-test("pan zoom", () => {
-  const state: CanvasState = {
-    mousedown: true,
-    scale: 1.0,
-    backgroundSize: 1.0,
-  };
-  const callback = vi.fn();
-  const onPanChange = vi.fn();
-  const touch: React.Touch = {screenX: 0, screenY: 0, idetifier: "touch", target: vi.fn(), clientX: 0, clientY: 0, pageX: 0, pageY: 0}
-  const touches: React.TouchList = {
-    length: 1,
-    item: vi.fn(),
-    identifiedTouch: vi.fn(),
-    1: touch,
-  }
-  CanvasController.panZoom(state, callback, onPanChange, touches);
-  expect(onPanChange).toHaveBeenCalledOnce();
-  expect(onPanChange).toHaveBeenCalledWith(0)
-  expect(callback).toHaveBeenCalledTimes(1);
-  expect(callback).toHaveBeenCalledWith({
-    mousedown: true,
-    scale: 1.0,
-    backgroundSize: 1.0,
-    lastTouch: touches,
-    initialTouch: touches,
+describe("move", () => {
+  test("zero to ten", () => {
+    const state: CanvasState = {
+      mousedown: true,
+      scale: 1.0,
+      backgroundSize: 1.0,
+    };
+    const point: Point = { x: 10, y: 10 };
+    const origin: Point = { x: 0, y: 0 };
+    const callback = vi.fn();
+    CanvasController.move(state, callback, point, origin);
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith({
+      mousedown: true,
+      scale: 1.0,
+      backgroundSize: 1.0,
+      translate: {
+        x: 10,
+        y: 10,
+      },
+    });
+  });
+
+  test("ten to zero", () => {
+    const state: CanvasState = {
+      mousedown: true,
+      scale: 1.0,
+      backgroundSize: 1.0,
+    };
+    const point: Point = { x: 0, y: 0 };
+    const origin: Point = { x: 10, y: 10 };
+    const callback = vi.fn();
+    CanvasController.move(state, callback, point, origin);
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith({
+      mousedown: true,
+      scale: 1.0,
+      backgroundSize: 1.0,
+      translate: {
+        x: -10,
+        y: -10,
+      },
+    });
+  });
+
+  test("zero to twenty in two steps", () => {
+    const state: CanvasState = {
+      mousedown: true,
+      scale: 1.0,
+      backgroundSize: 1.0,
+      translate: {
+        x: 10,
+        y: 10,
+      },
+      moveOrigin: {
+        x: 0,
+        y: 0,
+      },
+    };
+    const point: Point = { x: 10, y: 10 };
+    const origin: Point = { x: 0, y: 0 };
+    const callback = vi.fn();
+    CanvasController.move(state, callback, point, origin);
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith({
+      mousedown: true,
+      scale: 1.0,
+      backgroundSize: 1.0,
+      moveOrigin: {
+        x: 0,
+        y: 0,
+      },
+      translate: {
+        x: 20,
+        y: 20,
+      },
+    });
   });
 });
